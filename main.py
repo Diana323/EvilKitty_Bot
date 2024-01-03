e3a51cc5d10074470d990935456140d4202980b9
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2024-01-03 20:13:52 +0200

    запустила

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-#import os
-
-#import telebot
-
-#BOT_TOKEN = os.environ.get('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
-
-#bot = telebot.TeleBot(BOT_TOKEN)
-#@bot.message_handler(commands=['start', 'hello'])
-#def send_welcome(message):
-#    bot.reply_to(message, "Howdy, how are you doing?")
-#@bot.message_handler(func=lambda msg: True)
-#def echo_all(message):
-#    bot.reply_to(message, message.text)
 import main
 bot=main.TeleBot('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
 
