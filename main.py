1f9e4096fe13b4b3fb7a64447a1cc91b9b7c8080
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2024-01-03 22:15:40 +0200

    ////

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import main
-bot=main.TeleBot('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
+import telebot
+
+bot=telebot.TeleBot('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет,{message.from_user.first_name}'
-    bot.send_message(message.chat.id, mess, parse_mode='html')
+    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
 
 bot.polling(none_stop=True)
