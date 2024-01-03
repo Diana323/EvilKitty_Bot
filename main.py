342e0fd659892f58127da1be6d4241733c524b29
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2024-01-03 20:11:59 +0200

    .....

--- main.py
+++ main.py
@@ -11,10 +11,12 @@
 #@bot.message_handler(func=lambda msg: True)
 #def echo_all(message):
 #    bot.reply_to(message, message.text)
-import telebot
-bot=telebot.TeleBot('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
+import main
+bot=main.TeleBot('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
 
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'привет, для заказа товара нажмите <a>заказать</a>', parse_mode='html')
-    bot.polling(none_stop=True)
+    mess = f'привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
+bot.polling(none_stop=True)
