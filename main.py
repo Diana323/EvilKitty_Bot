379daed5b322e86a33d5fdab936a8b5026254b03
Діана Пахалюк <110489496+Diana323@users.noreply.github.com>
2024-01-04 04:03:15 +0200

    устранение ошибок

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import telebot
 
-bot=telebot.TeleBot('6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc')
+bot = telebot.TeleBot("6677943038:AAFthSBobYWh-QTjTTpNN3ygtq_X4LxfAIc")
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
+    mess = f"Привет, <b>{message.from_user.first_name}</b>"
+    bot.send_message(message.chat.id, mess, parse_mode="html")
+
+@bot.message_handler()
+def get_user_text(message):
+    bot.send_message(message.chat.id, message, parse_mode="html")
 
 bot.polling(none_stop=True)
